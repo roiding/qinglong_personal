e5d9d8916b674d90954ba43e6e492ed6822f0d9e
roiding <dingran@ran-ding.ga>
2026-02-27 18:14:40 +0800

    更新签到脚本

--- glados_signin.py
+++ glados_signin.py
@@ -4,10 +4,20 @@ cron: 30 8 * * *
 '''
 import os
 import requests
-glados_token=os.environ.get("glados_token","9037725680843507643281407025606-900-1440")
-glados_cookies=os.environ.get("glados_cookies","koa:sess=eyJjb2RlIjoiSjlDMTktTVE0WVctUEUwRzktNjJGUDMiLCJ1c2VySWQiOjY1MDY1NiwiX2V4cGlyZSI6MTc4Njk2MTI4NTE3NiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=AEgLaqvobB8afqPPgCNhqfkeRs4")
-response=requests.post("https://glados.space/api/user/checkin",json={"token":"glados.one"},headers={
-    "Authorization" : glados_token,
-    "Cookie" : glados_cookies,
-    "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
-}).json()
+import json
+# glados_token=os.environ.get("glados_token","")
+glados_cookies=os.environ.get("glados_cookies","")
+# 把字符串转数组 "["",""]"
+cookies_list=json.loads(glados_cookies)
+for cookie in cookies_list:
+    requests.post("https://glados.cloud/api/user/checkin",json={"token":"glados.cloud"},headers={
+        # "Authorization" : glados_token,
+        "Cookie" : glados_cookies,
+        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+    })
+    result=requests.get("https://glados.cloud/api/user/status",headers={
+        "Cookie" : glados_cookies,
+        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+    }).json()
+    print(f"{result['email']}签到成功！")
+    
